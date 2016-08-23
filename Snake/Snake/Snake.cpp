c745a76bb02e7e2d13e784c85ea264a8c37d1546
meakambut <ol.grit@gmail.com>
2016-08-23 03:01:27 +0300

    hello, world

---
+++ Snake/Snake/Snake.cpp
@@ -0,0 +1,16 @@
+// Snake.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+
+int main()
+{
+	cout << "hello, world!\n";
+	cout << cin.get();
+    return 0;
+}
+
